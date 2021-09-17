bf080453c1b73b9d898c5398a46f5b896ebc4783
Arseny Sazonov <gwentsenya@gmail.com>
2021-09-17 14:00:54 +0500

    Some mistakes

--- src/main/java/com/conditionalNetology/ConditionalApp/Config.java
+++ src/main/java/com/conditionalNetology/ConditionalApp/Config.java
@@ -2,7 +2,9 @@ package com.conditionalNetology.ConditionalApp;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class Config {
     @Bean
     @ConditionalOnProperty(name = "netology.profile.dev", havingValue = "true")
